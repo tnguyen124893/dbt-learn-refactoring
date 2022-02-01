c374a659f9b8a74656e499d3053240ee82f29c9b
Tung Nguyen <nguyensontung5794@gmail.com>
2022-02-01 22:55:01 +0700

    add ref to staging models

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -3,18 +3,18 @@ with
 import_orders as (
     select
         *
-    from {{source('dbt_tung','orders')}}
+    from {{ref('stg_jaffle_shop__orders')}}
 )
 , import_customers as (
     select
         *
-    from {{source('dbt_tung','customers')}}
+    from {{ref('stg_jaffle_shop__customers')}}
 )
 
 , import_payments as (
     select
         *
-    from {{source('dbt_tung','payments')}}
+    from {{ref('stg_stripe__payments')}}
     where 1=1
     and status <> 'fail'
 )
@@ -30,18 +30,18 @@ import_orders as (
         p.payment_finalized_date,
         c.first_name as customer_first_name,
         c.last_name as customer_last_name
-from import_orders as orders
-left join (
-    select 
-        orderid as order_id, 
-        max(created) as payment_finalized_date, 
-        sum(amount) / 100.0 as total_amount_paid
+    from import_orders as orders
+    left join (
+        select 
+            orderid as order_id, 
+            max(created) as payment_finalized_date, 
+            sum(amount) / 100.0 as total_amount_paid
         from import_payments
-    group by 1
-) p 
-    on orders.id = p.order_id
-left join import_customers c 
-    on orders.user_id = c.id 
+        group by 1
+        ) p 
+        on orders.id = p.order_id
+    left join import_customers c 
+        on orders.user_id = c.id 
 )
 
 , customer_orders as (
@@ -50,39 +50,41 @@ left join import_customers c
         , min(order_date) as first_order_date
         , max(order_date) as most_recent_order_date
         , count(orders.id) as number_of_orders
-from import_customers c 
-left join import_orders as orders
-    on orders.user_id = c.id 
-group by 1
+    from import_customers c 
+    left join import_orders as orders
+        on orders.user_id = c.id 
+    group by 1
 )
 ---Final
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-    case 
-        when c.first_order_date = p.order_placed_at then 'new'
-        else 'return' 
-        end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c 
-    using (customer_id)
-left outer join (
+, final as (
     select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
+        p.*,
+        row_number() over (order by p.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+        case 
+            when c.first_order_date = p.order_placed_at then 'new'
+            else 'return' 
+            end as nvsr,
+        x.clv_bad as customer_lifetime_value,
+        c.first_order_date as fdos
     from paid_orders p
-    left join paid_orders t2 
-        on p.customer_id = t2.customer_id 
-        and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x 
-    on x.order_id = p.order_id
-order by order_id
+    left join customer_orders as c 
+        using (customer_id)
+    left outer join (
+        select
+            p.order_id,
+            sum(t2.total_amount_paid) as clv_bad
+        from paid_orders p
+        left join paid_orders t2 
+            on p.customer_id = t2.customer_id 
+            and p.order_id >= t2.order_id
+        group by 1
+        order by p.order_id
+    ) x 
+        on x.order_id = p.order_id
+    order by order_id
+)
 ---Simple Select
-
-
-
+select
+    *   
+from final
